c61bc3b4c9cfb65534a81121a605830002f2c62a
ReallyRad <arthur.pointeau@gmail.com>
2019-12-12 16:23:58 +0100

    update to rxjs async observables

--- src/app/esri-map/esri-map.component.ts
+++ src/app/esri-map/esri-map.component.ts
@@ -13,41 +13,58 @@
 
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { loadModules } from 'esri-loader';
+import { EsriMapService } from '../services/esri-map.service';
 
 @Component({
   selector: 'app-esri-map',
   templateUrl: './esri-map.component.html',
   styleUrls: ['./esri-map.component.css']
 })
-
 export class EsriMapComponent implements OnInit {
 
-  // this is needed to be able to create the MapView at the DOM element in this component
-  @ViewChild('mapViewNode') private mapViewEl: ElementRef;
+  @ViewChild('mapViewNode') private viewNode: ElementRef; // needed to inject the MapView into the DOM
+  mapView: __esri.MapView;
+  panRequestSubscription: any;
 
-  constructor() { }
+  constructor(private mapService: EsriMapService) {}
 
-  ngOnInit() {
-    loadModules([
+  panMap(coordinates){
+    this.mapView.goTo(coordinates)
+    .then(() => {
+      this.mapView.zoom = 18;
+      setTimeout(() => {
+        this.mapService.panToWonderComplete();
+      }, 2000);
+    });
+  }
+
+  public ngOnInit() {
+
+    this.panRequestSubscription = this.mapService.panRequest.subscribe(() => {
+      this.panMap(this.mapService.wonderCoordinates);
+    });
+
+    // use esri-loader to load JSAPI modules
+    return loadModules([
       'esri/Map',
-      'esri/views/SceneView'
+      'esri/views/SceneView',
+      'esri/Graphic'
     ])
-      .then(([EsriMap, EsriSceneView]) => {
-        const map = new EsriMap({
-          basemap: 'streets',
-          ground: 'world-elevation'
+      .then(([Map, MapView, Graphic]) => {
+        const map: __esri.Map = new Map({
+          basemap: 'hybrid',
+          ground: "world-elevation"
         });
 
-        const mapView = new EsriSceneView({
-          container: this.mapViewEl.nativeElement,
-          center: [0.1278, 51.5074],
-          zoom: 10,
+        this.mapView = new MapView({
+          container: this.viewNode.nativeElement,
+          center: [-12.287, -37.114],
+          zoom: 12,
           map: map
         });
       })
       .catch(err => {
-        console.error(err);
+        console.log(err);
       });
   }
-
 }
