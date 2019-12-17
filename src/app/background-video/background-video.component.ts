c1fbb18ae86c006ba70264ed4f77acc46f3c06ca
ReallyRad <arthur.pointeau@gmail.com>
2019-12-17 11:22:26 +0100

    add background video component

---
+++ src/app/background-video/background-video.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-background-video',
+  templateUrl: './background-video.component.html',
+  styleUrls: ['./background-video.component.scss']
+})
+export class BackgroundVideoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
