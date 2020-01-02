e13e72726ca525a89cdd2102f173a40af8d96cab
ReallyRad <arthur.pointeau@gmail.com>
2020-01-02 11:58:14 +0100

    load media header carousel images dynamically

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import {SpreadsheetService} from '../HomePage/spreadsheet.service';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-carousel',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarouselComponent implements OnInit {
 
-  constructor() { }
+  carouselImages: string[]
+
+  constructor(private spreadSheetJSONServiceVariable: SpreadsheetService,
+              private route: ActivatedRoute,
+              ) {
+  }
 
   ngOnInit() {
+    this.carouselImages = this.spreadSheetJSONServiceVariable.GetRowByID(this.route.snapshot.paramMap.get('id')).carouselimages.split(',')
+    console.log(this.carouselImages)
   }
 
 }
