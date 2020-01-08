600b7850d190fa2ed23288c01feadbe6285e5f8d
Adrian <dhdeveloper@eodyne.com>
2020-01-08 17:00:03 +0100

    Update funcionality story principal page

--- src/app/story-PrincipalPage/story-PrincipalPage.component.ts
+++ src/app/story-PrincipalPage/story-PrincipalPage.component.ts
@@ -9,12 +9,35 @@ import { ActivatedRoute } from "@angular/router";
 })
 export class StoryPrincipalPage implements OnInit {
 
+  tableStories : Array<any> = [];
+  tableBlock1 : Array<any> = [];
+  tableBlock2 : Array<any> = [];
+  tableBlock3 : Array<any> = [];
+
   constructor(private spreadSheetServiceVariable: SpreadsheetService, private route: ActivatedRoute) {
 
   }
 
   ngOnInit() {
-    this.route.data.subscribe((data:any) => console.log(data));
+    
+    //this.route.data.subscribe((data:any) => console.log(data));
+    this.tableStories = this.spreadSheetServiceVariable.DBStory;
+
+    for(let i = 0; i < this.tableStories.length; i++)
+    {      
+      if(this.tableStories[i].inblockact == "1") 
+      {        
+        this.tableBlock1.push(this.tableStories[i]);
+      }
+      else if(this.tableStories[i].inblockact == "2")
+      {
+        this.tableBlock2.push(this.tableStories[i]);
+      }
+      else if(this.tableStories[i].inblockact == "3")
+      {
+        this.tableBlock3.push(this.tableStories[i]);
+      }
+    }
   }
 
 }
