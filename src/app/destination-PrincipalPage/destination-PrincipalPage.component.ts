a5dd1329709d43fbb1bf4e84e7530e9171617c06
Adrian <dhdeveloper@eodyne.com>
2020-01-08 16:58:48 +0100

    Update destination principalpage

--- src/app/destination-PrincipalPage/destination-PrincipalPage.component.ts
+++ src/app/destination-PrincipalPage/destination-PrincipalPage.component.ts
@@ -9,12 +9,36 @@ import { ActivatedRoute } from "@angular/router";
 })
 export class DestinationPrincipalPage implements OnInit {
 
+  tableDestination : Array<any> = [];
+  tableBlock1 : Array<any> = [];
+  tableBlock2 : Array<any> = [];
+  tableBlock3 : Array<any> = [];
+
   constructor(private spreadSheetServiceVariable: SpreadsheetService, private route: ActivatedRoute) {
 
   }
 
   ngOnInit() {
-    this.route.data.subscribe((data:any) => console.log(data));
+
+        //this.route.data.subscribe((data:any) => console.log(data));
+        this.tableDestination = this.spreadSheetServiceVariable.DBDestination;
+
+        for(let i = 0; i < this.tableDestination.length; i++)
+        {      
+          if(this.tableDestination[i].inblockact == "1") 
+          {        
+            this.tableBlock1.push(this.tableDestination[i]);
+          }
+          else if(this.tableDestination[i].inblockact == "2")
+          {
+            this.tableBlock2.push(this.tableDestination[i]);
+          }
+          else if(this.tableDestination[i].inblockact == "3")
+          {
+            this.tableBlock3.push(this.tableDestination[i]);
+          }
+        }
+
   }
 
 }
