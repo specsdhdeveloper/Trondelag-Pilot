1663a23abd11f414742c787713aaac07a448e522
ReallyRad <arthur.pointeau@gmail.com>
2020-01-21 16:03:47 +0100

    put img url in cardImg url

--- src/app/services/spreadsheet.service.ts
+++ src/app/services/spreadsheet.service.ts
@@ -16,15 +16,32 @@ export class SpreadsheetService {
         activities: []
     }
 
+    apiUrl = 'http://54.228.244.252:1337/'
+
+    imgFields = ['cardImg', 'imgLeft', 'imgRight', 'imgLeftBottom']
+
     public getJSON(url, table): Observable<any> {
         return this.http.get(url + table)
             .pipe(
                 map((res: any) => {
-                        console.log(res)
-                        const data = res
-                        this.tables[table] = data
-                        return data;
+                    const data = res;
+                    const returnArray = new Array;
+                    if (data && data.length > 0) {
+                        data.forEach(entry => {
+                            const obj = {};
+                            for (const x in entry) {
+                                if (x == 'cardImg' && entry['cardImg'] != null) { //replaces cardImg field with the url of the image
+                                    obj[x] = this.apiUrl + entry['cardImg'].url
+                                } else {
+                                    obj[x] = entry[x]
+                                }
+                            }
+                            returnArray.push(obj);
+                        });
                     }
+                    this.tables[table] = returnArray;
+                    return returnArray;
+                }
                 )
             );
     }
@@ -33,8 +50,7 @@ export class SpreadsheetService {
 
         const calls = [];
         Object.keys(this.tables).forEach(table => {
-            console.log("getting " + 'http://localhost:1337/' + table)
-            calls.push( this.getJSON('http://localhost:1337/', table));
+            calls.push( this.getJSON(this.apiUrl, table));
         })
 
         return forkJoin(calls);
