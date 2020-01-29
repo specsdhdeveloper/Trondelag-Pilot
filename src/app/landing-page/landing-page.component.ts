92df843a9ae62dd7c351ffb3c1f49b0c2b6ee16d
Adrian <dhdeveloper@eodyne.com>
2020-01-29 13:35:42 +0100

    proves deploy cache video

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -19,10 +19,43 @@ export class LandingPageComponent implements OnInit {
 
   }
 
+  /*loadVideo(url) {
+    return new Promise((resolve, reject) => { // here we download it entirely
+        let request = new XMLHttpRequest();
+        request.responseType = 'blob';
+        request.onload = (evt)=>resolve(request.response);
+        request.onerror = reject;
+        request.open('GET', url);
+        request.send();
+      }).then((blob)=> 
+          new Promise((resolve, reject)=>{
+              resolve(URL.createObjectURL(blob)); // return the blobURL directly
+              })
+          );
+  
+  }*/
+
+  loadVideo(url) {
+    return fetch(url)
+      .then(resp => resp.blob())
+      .then(blob => URL.createObjectURL(blob));
+  }
+
   ngOnInit() {
     //"https://specs.owncube.com/index.php/s/dCM64UvY79Fj1vr/download?path=%2F&files=Overview-cropped_h264-800p.mp4"
     this.route.data.subscribe((data:any) => console.log(data));
     window.scrollTo(0, 0);
+
+
+    
+    this.loadVideo('https://specs.owncube.com/index.php/s/dCM64UvY79Fj1vr/download?path=%2F&files=Overview-cropped_h264-800p.mp4')
+      .then(blobUrl => { // now it's loaded
+      console.log('entra');
+        document.body.className = 'loaded';
+        let vid = document.querySelector('video');
+        vid.src = blobUrl; // we just set our mediaElement's src to this blobURL
+        vid.onload = () => URL.revokeObjectURL(blobUrl);
+      }).catch((err) => console.log(err));
   }
 
 }
